49234751b3e87fb0d6064cbce80ea0737c399abc
Codecrafter404 <60842859+codecrafter404@users.noreply.github.com>
2025-10-09 13:14:49 +0200

    kairos bundle

---
+++ kairos_bundle/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -rf /system/discovery/kcrypt-discovery-challenger
+cp assets/kcrypt-discovery-challenger /system/discovery/kcrypt-discovery-challenger
+chmod 755 /system/discovery/kcrypt-discovery-challenger
