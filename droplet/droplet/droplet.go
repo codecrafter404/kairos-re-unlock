70a454e795aeb02b43156986d35f3ad42f4aebb1
Codecrafter404 <60842859+codecrafter404@users.noreply.github.com>
2025-12-13 12:21:31 +0100

    implement http on droplet

--- droplet/droplet/droplet.go
+++ droplet/droplet/droplet.go
@@ -1,82 +1,23 @@
 package droplet
 
 import (
-	"context"
-	"fmt"
 	"os"
-	"time"
 
-	"github.com/codecrafter404/go-nodepair"
-	"github.com/codecrafter404/kairos-re-unlock/common"
+	"github.com/codecrafter404/kairos-re-unlock/droplet/config"
+	"github.com/codecrafter404/kairos-re-unlock/droplet/provider"
 	"github.com/kairos-io/kcrypt/pkg/bus"
 	"github.com/mudler/go-pluggable"
 	"github.com/rs/zerolog/log"
 )
 
-func Start(config Config) error {
+func Start(config config.Config) error {
 	log.Debug().Any("config", config).Msg("Starting discovery")
 	factory := pluggable.NewPluginFactory()
 
 	// Input: bus.EventInstallPayload
 	// Expected output: map[string]string{}
 	factory.Add(bus.EventDiscoveryPassword, func(e *pluggable.Event) pluggable.EventResponse {
-
-		var err_s string
-
-		data_s, err := getResponse(config)
-
-		if err != nil {
-			err_s = err.Error()
-		}
-
-		// respond with password
-		return pluggable.EventResponse{
-			Data:  data_s,
-			Error: err_s,
-		}
+		return provider.GetResponse(config)
 	})
 	return factory.Run(pluggable.EventType(os.Args[1]), os.Stdin, os.Stdout)
-}
-
-// TODO: add retry logic
-func getResponse(config Config) (string, error) {
-	// edgevpn get payload
-	ctx, _ := context.WithCancel(context.Background())
-	payload := &common.Payload{}
-
-	log.Info().Msg("Waiting for payload")
-	err := nodepair.Receive(ctx, payload, nodepair.WithToken(config.EdgeVPNToken))
-
-	if err != nil {
-		log.Err(err).Msg("Failed to receive payload")
-		return "", err
-	}
-	log.Info().Any("payload", payload).Msg("Payload received")
-
-	// check signature
-	err = payload.IsValidSignature(config.PublicKey)
-
-	if err != nil {
-		log.Err(err).Msg("Signature verification failed")
-		return "", err
-	}
-
-	// check timestamp
-	now := time.Now().Unix()
-	var gracePeriod int64 = 60 * 15
-
-	if payload.Timestamp < (now-gracePeriod) || payload.Timestamp > now {
-		err = fmt.Errorf("Invalid timestamp")
-		log.Err(err).Int64("grace", gracePeriod).Int64("now", now).Msg("Invalid timestamp")
-		return "", err
-	}
-
-	// decrypt payload
-	password, err := payload.DecryptPassword(config.PrivateKey)
-	if err != nil {
-		log.Err(err).Msg("Decryption failed")
-		return "", nil
-	}
-
-	return password, nil
 }
